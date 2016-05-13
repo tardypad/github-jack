3f9bfcb03b63d6689e6ba904ac595e4e7a939136
Damien Tardy-Panis <damien@tardypad.me>
2016-05-13 21:18:23 +0200

    add function commit_day_work $date $count

--- work.sh
+++ work.sh
@@ -4,6 +4,25 @@ REPO="$1"
 MESSAGE='All work and no play makes Jack a dull boy.'
 AUTHOR='Jack <jack@work.com>'
 
+function random_time()
+{
+  local hours=$(( $RANDOM % 24 ))
+  local minutes=$(( $RANDOM % 60 ))
+  local seconds=$(( $RANDOM % 60 ))
+  echo $hours:$minutes:$seconds
+}
+
+function commit_day_work()
+{
+  local day="$1"
+  local count="$2"
+
+  for ((i = 1; i <= $count; i++))
+  do
+    commit_a_work "$day" "$(random_time)"
+  done
+}
+
 function commit_a_work()
 {
   local date="$1"
@@ -20,4 +39,4 @@ function commit_a_work()
   --quiet
 }
 
-commit_a_work 2016-05-01 12:00:00
+commit_day_work 2016-05-01 3
